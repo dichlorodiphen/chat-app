68e6f614ff718edc183002604c5e8f1e5e0ab24f
David Young <dichlorodiphen@gmail.com>
2023-09-18 23:21:38 -0400

    Add deployment to build.sh

--- build.sh
+++ build.sh
@@ -19,3 +19,17 @@ cd ..
 minikube kubectl -- apply -Rf deployment/
 minikube kubectl -- get pods
 
+# wait for database to deploy
+echo 'waiting for database to deploy'
+sleep 20s
+echo 'database deployed, now streaming backend logs'
+
+# stream logs from backend
+pods=$(minikube kubectl -- get pods)
+pattern='(backend[0-9a-z-]+)'
+if [[ $pods =~ $pattern ]]; then
+  minikube kubectl -- logs -f ${BASH_REMATCH[0]}
+else
+  echo "unexpected error"
+fi
+
